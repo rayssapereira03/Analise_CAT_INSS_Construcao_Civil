3caa37fcfab17c307476835a78b2fd76d0fb80f0
rayssapereira03 <rayssapereiradasilvaa@gmail.com>
2026-08-11 21:33:12 -0300

    análise de acidentes na construção civil

--- CAT_CONST_CIVIL_26.R
+++ CAT_CONST_CIVIL_26.R
@@ -1,9 +1,11 @@
 ##     ANALISANDO DADOS DO CAT: JAN - MAi 2026 (CONSTRUÇÃO CIVIL)
 
-#iniciando pacotes
+# iniciando pacotes
 library(dplyr)
 library(readr)
 library(ggplot2)
+#install.packages("lubridate")
+library(lubridate)
 
 # leitura do arquivo
 
@@ -25,16 +27,16 @@ mai <- read_delim("cat_mai26.csv",
 
 dados <- bind_rows(jan, fev, mar, abr, mai)
 
-#visualisando base de dados
+# visualisando base de dados
 View(dados)
 
-#consultando as colunas - varíaveis
+# consultando as colunas - varíaveis
 names(dados)
 
-#resumo estatístico das colunas
+# resumo estatístico das colunas
 summary(dados)
 
-#tranformando profissioes em categorias
+# tranformando profissões em categorias
 dados <- dados |>
   mutate(
     CBO...4 = as.factor(CBO...4)
@@ -72,31 +74,35 @@ construcao <- construcao |>
 # resumo estatítico das profissiões 
 summary(construcao)
 
-## Quais ocupações aparecem com mais frequência? 
+## Qual profissão possui o maior número de casos de acidentes durante os meses análisados?
 
-# criando gráfico - GRÁFICO DE BARRAS SIMPLES
-install.packages("lubridate")
-library(lubridate)
 
 # criando a coluna de meses
 construcao$mes <- month(construcao$`Data Acidente...23`,
                         label = TRUE,
                         abbr = FALSE)
 
-# gráfico ocorrencias de acidentes por profissão em cada mês
+# gráfico: ocorrencias de acidentes por profissão em cada mês
 ggplot(construcao,
        aes(y = profissao,
            fill = mes)) +
   geom_bar() +
   labs(
-    title = "Ocorrências de acidentes por profissão",
+    title = "Ocorrências de acidentes por profissão entre Janeiro e Maio de 2026",
     x = "Quantidade de registros",
     y = "Profissão",
     fill = "Mês"
   ) +
   theme_classic()
 
+## CONCLUSÃO: A PROFISSÃO COM O MAIOR NÚMERO DE REGISTROS DE ACIDENTES ENTRE JANEIRO E MAIO DE 2026 FOI O SERVENTE DE OBRAS
+## REGISTRANDO CERCA DE 6 MIL CASOS
+## EM SEGUNDO LUGAR ESTÁ O PEDREIRO, COM CERCA DE 2.500 CASOS REGISTRADOS
+## ABRIL FOI O MÊS RECORDE DE ACIDENTES ENTRE OS MESES ANALISADOS
+
+
 ## Quais estados registraram mais acidentes e mortes?
+
 
 #tranformando dados em categorias
 construcao <- construcao |>
@@ -116,6 +122,7 @@ dados_estados <- construcao |>
 
 # alterando a variável ñ class para UF não informada
 dados_estados <- dados_estados |>
+  filter(`UF  Munic.  Acidente` != "Zerado") |>
   mutate(`UF  Munic.  Acidente` = as.character(`UF  Munic.  Acidente`),
     `UF  Munic.  Acidente` =
            ifelse(`UF  Munic.  Acidente` == "{ñ class}",
@@ -150,7 +157,14 @@ ggplot(dados_estados,
   
   theme_minimal()
 
-# Quantos acidentes existem para cada combinação de estado e ocupação?
+## CONCLUSÃO: ENTRE OS ESATADOS COM INFORMAÇÃO DISPONÍVEL
+## O MARANHÃO APRESENTA O MAIOR NÚMERO DE ACIDENTES
+## O MARANHÃO REGISTRA CERCA DE 3.500 CASOS DE ACIDENTES DURANTE O PERÍODO ANALISADO
+## JÁ CATEGORIA "UF NÃO INFORMADA" LIDERA NA QUANTIDADE DE REGISTROS
+## COM QUASE 4 MIL CASOS
+## OS ESTADOS COM MAIOR NÚMERO DE ÓBTIDOS SÃO: AMAZONAS(3,3%), PIAUÍ(2,6%) E CEARÁ(2,1%)
+
+## Como os acidentes se distribuem entre profissão e estado?
 heat <- construcao |>
   count(`UF  Munic.  Acidente`, profissao)
 View(heat)
@@ -184,7 +198,14 @@ ggplot(heat,
     axis.text.x = element_text(angle = 45, hjust = 1)
   )
 
-## qual parte do corpo é mais atingida nos acidentes?
+## CONCLUSÃO: OS DADOS CONFIRMAM AS ANÁLISES ANTERIORES
+## A PROFISSÃO COM MAIOR NÚMERO DE REGISTRO DE ACIDENTES
+## É O SERVENTE DE OBRA, ENTRE AS PROFISSÕES ANALISADAS
+## O MARANHÃO EVIDENCIA O MAIOR NÚMERO DE REGISTROS ENVOLVENDO SERVENTES
+## PEDREIROS APARECEM EM SEGUNDO LUGAR COM MAIOR NÚMERO DE CASOS DE ACIDENTES
+
+
+## Qual parte do corpo é mais atingida nos acidentes em canteiros de obra?
 
 # transformando parte do corpo em categoria
 construcao <- construcao |>
@@ -192,14 +213,8 @@ construcao <- construcao |>
     `Parte Corpo Atingida` = as.factor(`Parte Corpo Atingida`)
   )
 
-# usando count para contar as ocorrências e o sort para ordenar do maior para o menor
-parte_corpo <- construcao |>
-  count(`Parte Corpo Atingida`, sort = TRUE)
-
-summary(parte_corpo$`Parte Corpo Atingida`)
-
 # alterando o título das linhas
-parte_corpo <- parte_corpo |>
+construcao <- construcao |>
   mutate(
     parte_corpo_grafico = case_when(
       
@@ -248,7 +263,11 @@ parte_corpo <- parte_corpo |>
       TRUE ~ `Parte Corpo Atingida`
     )
   )
-summary(parte_corpo)
+
+# usando count para contar as ocorrências e o sort para ordenar do maior para o menor
+parte_corpo <- construcao |>
+  count(parte_corpo_grafico, sort = TRUE)
+
 
 ## criando o gráfico
 ggplot(parte_corpo, aes(x = reorder(parte_corpo_grafico, n), y = n)) +
@@ -261,6 +280,9 @@ ggplot(parte_corpo, aes(x = reorder(parte_corpo_grafico, n), y = n)) +
   )+
   theme_classic()
 
+## CONCLUSÃO: O DEDO É A PARTE DO CORPO MAIS ATINGIDA NOS ACIDENTES, COM MAIS DE 2.500 CASOS
+## PÉ E MÃO TAMBÉM POSSUEM REGISTROS SIGNIFICATIVOS COM 1.000 E 2.000 CASOS REGISTRADOS RESPECTIVAMENTE
+
 ## Agente causador × natureza da lesão
 
 # tranformando em categoria
@@ -269,62 +291,10 @@ construcao <- construcao |>
      `Natureza da Lesão` = as.factor(`Natureza da Lesão`)
   )
 
-# contagem de ocorrÊncias
-corpo_lesao <- construcao |>
-  count(`Parte Corpo Atingida`, `Natureza da Lesão`, sort = TRUE)
 
 # renomeando as varíaveis
-corpo_lesao <- corpo_lesao |>
-  mutate(
-    parte_corpo_grafico = case_when(
-      
-      `Parte Corpo Atingida` == "Abdome (Inclusive Or" ~ "Abdome",
-      `Parte Corpo Atingida` == "Antebraco (Entre O P" ~ "Antebraço",
-      `Parte Corpo Atingida` == "Aparelho Digestivo" ~ "Aparelho digestivo",
-      `Parte Corpo Atingida` == "Aparelho Genito-Urin" ~ "Aparelho geniturinário",
-      `Parte Corpo Atingida` == "Aparelho Respiratori" ~ "Aparelho respiratório",
-      `Parte Corpo Atingida` == "Artelho" ~ "Artelhos",
-      `Parte Corpo Atingida` == "Articulacao do Torno" ~ "Tornozelo",
-      `Parte Corpo Atingida` == "Boca (Inclusive Labi" ~ "Boca",
-      `Parte Corpo Atingida` == "Braco (Acima do Coto" ~ "Braço (acima do cotovelo)",
-      `Parte Corpo Atingida` == "Braco (Entre O Punho" ~ "Braço (entre cotovelo e punho)",
-      `Parte Corpo Atingida` == "Cabeca, Nic" ~ "Cabeça",
-      `Parte Corpo Atingida` == "Cabeca, Partes Multi" ~ "Cabeça (múltiplas)",
-      `Parte Corpo Atingida` == "Cotovelo" ~ "Cotovelo",
-      `Parte Corpo Atingida` == "Coxa" ~ "Coxa",
-      `Parte Corpo Atingida` == "Cranio (Inclusive En" ~ "Crânio",
-      `Parte Corpo Atingida` == "Dedo" ~ "Dedo",
-      `Parte Corpo Atingida` == "Dorso (Inclusive Mus" ~ "Dorso",
-      `Parte Corpo Atingida` == "Face, Partes Multipl" ~ "Face (múltiplas)",
-      `Parte Corpo Atingida` == "Joelho" ~ "Joelho",
-      `Parte Corpo Atingida` == "Localizacao da Lesao" ~ "Localização não informada",
-      `Parte Corpo Atingida` == "Mao (Exceto Punho ou" ~ "Mão",
-      `Parte Corpo Atingida` == "Mandibula (Inclusive" ~ "Mandíbula",
-      `Parte Corpo Atingida` == "Membros Inferiores," ~ "Membros inferiores",
-      `Parte Corpo Atingida` == "Membros Superiores," ~ "Membros superiores",
-      `Parte Corpo Atingida` == "Nariz (Inclusive Fos" ~ "Nariz",
-      `Parte Corpo Atingida` == "Olho (Inclusive Nerv" ~ "Olho",
-      `Parte Corpo Atingida` == "Ombro" ~ "Ombro",
-      `Parte Corpo Atingida` == "Ouvido (Externo, Med" ~ "Ouvido",
-      `Parte Corpo Atingida` == "Partes Multiplas - A" ~ "Partes múltiplas",
-      `Parte Corpo Atingida` == "Pe (Exceto Artelhos)" ~ "Pé",
-      `Parte Corpo Atingida` == "Perna (Do Tornozelo," ~ "Perna (tornozelo ao joelho)",
-      `Parte Corpo Atingida` == "Perna (Entre O Torno" ~ "Perna (joelho ao quadril)",
-      `Parte Corpo Atingida` == "Pescoco" ~ "Pescoço",
-      `Parte Corpo Atingida` == "Punho" ~ "Punho",
-      `Parte Corpo Atingida` == "Quadris (Inclusive P" ~ "Quadril",
-      `Parte Corpo Atingida` == "Sistema Musculo-Esqu" ~ "Sistema musculoesquelético",
-      `Parte Corpo Atingida` == "Sistema Nervoso" ~ "Sistema nervoso",
-      `Parte Corpo Atingida` == "Sistemas e Aparelhos" ~ "Sistemas e aparelhos",
-      `Parte Corpo Atingida` == "Torax (Inclusive Org" ~ "Tórax",
-      `Parte Corpo Atingida` == "Tronco, Nic" ~ "Tronco",
-      `Parte Corpo Atingida` == "Tronco, Parte Multip" ~ "Tronco (múltiplas)",
-      
-      TRUE ~ `Parte Corpo Atingida`
-    )
-  )
 
-corpo_lesao <- corpo_lesao |>
+construcao <- construcao |>
   mutate(
     natureza_grafico = case_when(
       
@@ -358,18 +328,21 @@ corpo_lesao <- corpo_lesao |>
     )
   )
 
-unique(corpo_lesao$`Natureza da Lesão`)
+# parte do corpo x natureza da lesão
+corpo_lesao <- construcao |>
+  count(parte_corpo_grafico, natureza_grafico, sort = TRUE)
 
 # o gráfico ficou poluído com muitas informações, vou selecionar o top 10 das lesões mais frequentes
-top_lesoes <- corpo_lesao |>
+top_lesoes <- construcao |>
   count(natureza_grafico, sort = TRUE) |>
   slice_head(n = 10)
 
+# selecionando somente as 10 lesões mais frequentes
 corpo_lesao_top <- corpo_lesao |>
-  filter(natureza_grafico %in% top_lesoes$natureza_grafico) |>
-  count(parte_corpo_grafico, natureza_grafico)
+  filter(natureza_grafico %in% 
+           top_lesoes$natureza_grafico) 
 
-# gráfico
+# gráfico 
 ggplot(
   corpo_lesao_top,
   aes(
@@ -393,6 +366,12 @@ ggplot(
     )
   )
 
+## CONCLUSÃO: FRATURAS APARECEM ENTRE OS PRINCIPAIS TIPOS
+## DE LESÃO REGISTRADOS
+## CORTE É O SEGUNDO TIPO DE LESÃO COM MAIOR NÚMERO DE REGISTROS
+## PORTANTO, DEDO É A PARTE DO CORPO MAIS LECIONADA NOS ACIDENTES REGISTRADOS
+## E OS CASOS MAIS RECORRENTES DE LESÕES SÃO FRATURAS E CORTES
+
 ## Profissão × agente causador
 
 # transformando em categoria
@@ -401,18 +380,14 @@ construcao <- construcao |>
     `Agente  Causador  Acidente` = as.factor(`Agente  Causador  Acidente`)
   )
 
-# contagem dos agentes causadores em cada profissão
-prof_agente <- construcao |>
-  count(profissao, `Agente  Causador  Acidente`)
 
-
-top_agentes <- prof_agente |>
-  arrange(desc(n)) |>
-  slice_head(n = 20)
+# top 10 dos agentes mais frequentes
+top_agentes <- construcao |>
+  count(`Agente  Causador  Acidente`, 
+        sort = TRUE) |>
+  slice_head(n = 10)
 
 # renomeando as varíaveis dos agentes causadores
-
-unique(top_agentes$`Agente  Causador  Acidente`)
 
 top_agentes <- top_agentes|>
   mutate(
@@ -439,10 +414,44 @@ top_agentes <- top_agentes|>
 
 View(top_agentes)
 
-# gráfico de barras - top 20 profissões com os agentes causadores mais frequentes 
+# agentes causadores x profissão
+prof_agente <- construcao |>
+  filter(
+    `Agente  Causador  Acidente` %in%
+      top_agentes$`Agente  Causador  Acidente`
+  ) |>
+  count(profissao, `Agente  Causador  Acidente`)
+
+# usando os nomes simplificados
+
+prof_agente <- prof_agente |>
+  mutate(
+    agentes_grafico = case_when(
+      `Agente  Causador  Acidente` == "Metal - Inclui Liga" ~ "Metal",
+      `Agente  Causador  Acidente` == "Chao - Superficie Ut" ~ "Chão/Superfície",
+      `Agente  Causador  Acidente` == "Rua e Estrada - Supe" ~ "Rua/Estrada",
+      `Agente  Causador  Acidente` == "Motocicleta, Motonet" ~ "Motocicleta",
+      `Agente  Causador  Acidente` == "Agente do Acidente," ~ "Agente do Acidente",
+      `Agente  Causador  Acidente` == "Madeira (Toro, Madei" ~ "Madeira",
+      `Agente  Causador  Acidente` == "Veiculo Rodoviario M" ~ "Veículo Rodoviário",
+      `Agente  Causador  Acidente` == "Martelo, Malho, Marr" ~ "Martelo",
+      `Agente  Causador  Acidente` == "Agente do Acidente I" ~ "Agente do Acidente I",
+      `Agente  Causador  Acidente` == "Andaime, Plataforma" ~ "Andaime/Plataforma",
+      `Agente  Causador  Acidente` == "Veiculo, Nic" ~ "Veículo (NIC)",
+      `Agente  Causador  Acidente` == "Produto Mineral nao" ~ "Produto Mineral",
+      `Agente  Causador  Acidente` == "Escada Movel ou Fixa" ~ "Escada Móvel/Fixa",
+      `Agente  Causador  Acidente` == "Piso de Edificio - S" ~ "Piso de Edifício",
+      `Agente  Causador  Acidente` == "Escada Permanente Cu" ~ "Escada Permanente",
+      TRUE ~ `Agente  Causador  Acidente`
+      
+    )
+  )
+
+
+# gráfico de barras - top 10 das profissões com os agentes causadores mais frequentes
 
 ggplot(
-  top_agentes,
+  prof_agente,
   aes(
     x = profissao,
     y = n,
@@ -463,3 +472,7 @@ ggplot(
       hjust = 1
     )
   )
+## CONCLUSÃO: O MAIOR AGENTE CAUSADOR DE ACIDENTES NOS CANTEIROS DE OBRA É O METAL
+## O SERVENTE SEGUE NA LIDERANÇA 
+## ENVOLVENDO OS PRINCIPAIS AGENTES CAUSADORES 
+## PEDREIRO APARECEM EM SEGUIDA ENTRE AS PROFISSÕES COM O MAIOR NÚMERO DE REGISTROS
